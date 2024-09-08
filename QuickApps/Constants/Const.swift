f817b47a8b1ecad1db65aef4826e1f4c843891fb
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2024-09-08 23:14:54 +0300

    Added Constants to project

---
+++ QuickApps/Constants/Const.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+// MARK: - Constants Enum
+
+enum Const {
+    static let compactMode = "Compact"
+    static let interactiveMode = "Interactive"
+    static let modeIcon = "square.grid.2x2"
+    static let miniAppCellReuseIdentifier = "miniAppCell"
+}
